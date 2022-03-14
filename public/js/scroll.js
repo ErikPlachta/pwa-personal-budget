01714fd1283182fa1aec02bced8c4c541c9b88b2
Erik Plachta <eplachta@gmail.com>
2022-03-13 22:33:34 -0400

    Add more complete styling to nav. ( still not done )

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -5,8 +5,11 @@ function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.getElementById("navbar").style.padding = "0px 10px";
     document.getElementById("logo").style.fontSize = "25px";
+    document.getElementById("logo-img").style.height = "30px";
+    
   } else {
     document.getElementById("navbar").style.padding = "10px 10px";
     document.getElementById("logo").style.fontSize = "35px";
+    document.getElementById("logo-img").style.height = "60px";
   }
 }
