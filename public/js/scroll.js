1118416d2427938f7b70285ed58159a1ebf5fc99
Erik Plachta <eplachta@gmail.com>
2022-03-14 17:19:54 -0400

    Add responsive styling to mobile 100%

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -2,17 +2,35 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("toolbar").style.top = "50px";
-    // document.getElementById("logo").style.fontSize = "25px";
-    document.getElementById("logo").style.fontSize = "var(--fs-600";
-    document.getElementById("logo-img").style.height = "30px";
-    document.getElementById("navbar").style.padding = "0px";
-    
-  } else {
-    document.getElementById("toolbar").style.top = "134px";
-    document.getElementById("logo").style.fontSize = "35px";
-    document.getElementById("logo-img").style.height = "50px";
-    document.getElementById("navbar").style.padding = "2rem 0";
+
+  if( document.documentElement.clientWidth > 750) {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("toolbar").style.top = "50px";
+      // document.getElementById("logo").style.fontSize = "25px";
+      document.getElementById("logo").style.fontSize = "var(--fs-600";
+      document.getElementById("logo-img").style.height = "30px";
+      document.getElementById("navbar").style.padding = "0px";
+      
+    } else {
+      document.getElementById("toolbar").style.top = "134px";
+      document.getElementById("logo").style.fontSize = "35px";
+      document.getElementById("logo-img").style.height = "50px";
+      document.getElementById("navbar").style.padding = "2rem 0";
+    }
+  }
+
+  else {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("toolbar").style.top = "50px";
+      document.getElementById("logo").style.fontSize = "30px";
+      document.getElementById("logo-img").style.height = "30px";
+      document.getElementById("navbar").style.padding = "0px";
+      
+    } else {
+      document.getElementById("toolbar").style.top = "134px";
+      document.getElementById("logo").style.fontSize = "35px";
+      document.getElementById("logo-img").style.height = "50px";
+      document.getElementById("navbar").style.padding = "0 0 2rem 0";
+    }
   }
 }
