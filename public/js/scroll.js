3919ce420c2f087fcfb9764b38eb95868aa26be6
Erik Plachta <eplachta@gmail.com>
2022-03-14 18:06:53 -0400

    Add complete about me styling. Add complete navigation animation

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -10,23 +10,27 @@ function scrollFunction() {
       document.getElementById("logo").style.fontSize = "var(--fs-600";
       document.getElementById("logo-img").style.height = "30px";
       document.getElementById("navbar").style.padding = "0px";
+      // document.getElementById("navbar-right").style.display = "none";
       
     } else {
       document.getElementById("toolbar").style.top = "134px";
       document.getElementById("logo").style.fontSize = "35px";
       document.getElementById("logo-img").style.height = "50px";
       document.getElementById("navbar").style.padding = "2rem 0";
+      // document.getElementById("navbar-right").style.display = "flex";
     }
   }
 
   else {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("navbar-right").style.display = "none";
       document.getElementById("toolbar").style.top = "50px";
       document.getElementById("logo").style.fontSize = "30px";
       document.getElementById("logo-img").style.height = "30px";
       document.getElementById("navbar").style.padding = "0px";
       
     } else {
+      document.getElementById("navbar-right").style.display = "flex";
       document.getElementById("toolbar").style.top = "134px";
       document.getElementById("logo").style.fontSize = "35px";
       document.getElementById("logo-img").style.height = "50px";
